ea02f3132edfdbd06b03b07be235f6d828fcfef1
Abdul Halim Chowdhury <salman@inverseai.com>
2023-11-17 23:51:59 +0600

    Buid rightbar

--- src/components/RightBar.tsx
+++ src/components/RightBar.tsx
@@ -1,16 +1,94 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  ImageList,
+  ImageListItem,
+  Typography,
+} from "@mui/material";
+
+import latest_photos from "../data/latest_photos.ts";
+import LatestConversation from "./LatestConversation.tsx";
 
 const RightBar = () => {
   return (
     <Box
       sx={{
-        backgroundColor: "red",
-        display: { xs: "none", sm: "block"},
+        display: { xs: "none", sm: "block" },
       }}
       flex={2}
       p={2}
     >
-      RightBar
+      <Box position="fixed" width={300}>
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={7}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://mui.com/static/images/avatar/5.jpg"
+          />
+          <Avatar alt="Remy Sharp" />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+          />
+          <Avatar
+            alt="Remy Sharp"
+            src="https://mui.com/static/images/avatar/1.jpg"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://mui.com/static/images/avatar/5.jpg"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://mui.com/static/images/avatar/5.jpg"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://mui.com/static/images/avatar/5.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://mui.com/static/images/avatar/5.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://mui.com/static/images/avatar/5.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://mui.com/static/images/avatar/5.jpg"
+          />
+        </AvatarGroup>
+        <Typography variant="h6" fontWeight={100} mt={3} mb={2}>
+          Latest Photos
+        </Typography>
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          {latest_photos.map((photo) => (
+            <ImageListItem key={photo.img}>
+              <img src={photo.img} alt={photo.title} loading="lazy" />
+            </ImageListItem>
+          ))}
+        </ImageList>
+        <Typography variant="h6" fontWeight={100} mt={3}>
+          Latest Conversations
+        </Typography>
+        <LatestConversation />
+      </Box>
     </Box>
   );
 };
---
+++ src/data/latest_photos.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  }
+];
