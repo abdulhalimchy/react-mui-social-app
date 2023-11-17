ba67febcdfff904f75a4769cf0bc3aed8d95d92d
Abdul Halim Chowdhury <salman@inverseai.com>
2023-11-17 20:34:30 +0600

    Style Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,46 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { styled } from "@mui/material/styles";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import SearchInput from "./SearchInput.tsx";
+import MailIcon from "@mui/icons-material/Mail";
+import Notifications from "@mui/icons-material/Notifications";
+import { useState } from "react";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
 
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  gap: "14px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "5px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <AppBar position="sticky">
       <StyledToolbar>
@@ -25,7 +57,46 @@ const Navbar = () => {
           </Typography>
         </Box>
         <SearchInput />
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            alt="Salman Chy"
+            src="https://mui.com/static/images/avatar/2.jpg"
+            onClick={() => setOpen(true)}
+          />
+        </Icons>
+        <UserBox onClick={() => setOpen(true)}>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            alt="Salman Chy"
+            src="https://mui.com/static/images/avatar/2.jpg"
+          />
+          <Typography>Salman</Typography>
+        </UserBox>
       </StyledToolbar>
+      <Menu
+        open={open}
+        onClose={() => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>Change Password</MenuItem>
+        <MenuItem>Settings</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
